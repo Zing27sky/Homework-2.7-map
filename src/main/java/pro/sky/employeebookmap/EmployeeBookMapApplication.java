8fea68a90dcf3e6013291011e201827a911eae10
Zing27sky <Zing27@mail.ru>
2024-02-26 23:25:13 +0300

    initial commit

---
+++ src/main/java/pro/sky/employeebookmap/EmployeeBookMapApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employeebookmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBookMapApplication.class, args);
+	}
+
+}
